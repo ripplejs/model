3f2e6811d6d12ba962b5eff024eeac446a75a400
Anthony Short <antshort@gmail.com>
2014-01-20 22:45:09 -0800

    Original API

--- index.js
+++ index.js
@@ -5,91 +5,92 @@ function toArray(val) {
   return val;
 }
 
-function Observable(data){
-  this.attributes = {};
-  Observable.emit('construct', this);
-  if(data) this.set(data);
-};
+module.exports = function(){
 
-emitter(Observable);
-emitter(Observable.prototype);
-
-Observable.computed = function(key, dependencies, fn) {
-  this.on('construct', function(model){
-    function update() {
-      model.set(key, fn.call(model));
-    }
-    fn.call(model);
-    model.change(dependencies, update);
-  });
-  return this;
-};
-
-Observable.prototype.change = function(key, fn) {
-  if(arguments.length === 2) {
-    var self = this;
-    var attrs = toArray(key);
-    function change(attr, value) {
-      if(attrs.indexOf(attr) === -1) return;
-      fn.call(self, attr, value);
-    }
-  }
-  else {
-    change = key.bind(this);
-  }
-  this.on('change', change);
-  return function(){
-    self.off('change', change);
+  function Observable(data){
+    this.attributes = {};
+    Observable.emit('construct', this);
+    if(data) this.set(data);
   };
-};
 
-Observable.prototype.set = function(key, val) {
-  if( typeof key !== "string" ) {
-    for(var name in key) {
-      this.set(name, key[name]);
-    }
+  emitter(Observable);
+  emitter(Observable.prototype);
+
+  Observable.computed = function(key, dependencies, fn) {
+    this.on('construct', function(model){
+      function update() {
+        model.set(key, fn.call(model));
+      }
+      fn.call(model);
+      model.change(dependencies, update);
+    });
     return this;
-  }
-  var previous = this.get(key);
-  if( previous === val ) return; // No change
-  this._setPath(key, val);
-  this.emit('change', key, val, previous);
-  this.emit('change'+key, val, previous);
-  return this;
-};
+  };
 
-Observable.prototype._setPath = function(path, value) {
-  var parts = path.split('.');
-  var target = this.attributes;
-  var last = parts.pop();
-  while(parts.length) {
-    part = parts.shift();
-    if(!target[part]) {
-      target[part] = {};
+  Observable.prototype.change = function(key, fn) {
+    if(arguments.length === 2) {
+      var self = this;
+      var attrs = toArray(key);
+      function change(attr, value) {
+        if(attrs.indexOf(attr) === -1) return;
+        fn.call(self, attr, value);
+      }
     }
-    target = target[part];
-  }
-  target[last] = value;
-  this.emit('change:'+path, value, previousValue);
-};
+    else {
+      change = key.bind(this);
+    }
+    this.on('change', change);
+    return function(){
+      self.off('change', change);
+    };
+  };
 
-Observable.prototype._getPath = function(path) {
-  var parts = path.split('.');
-  var value = this.attributes;
-  while(parts.length) {
-    var part = parts.shift();
-    value = value[part];
-    if(value === undefined) parts.length = 0;
-  }
-  return value;
-};
+  Observable.prototype.set = function(key, val) {
+    if( typeof key !== "string" ) {
+      for(var name in key) {
+        this.set(name, key[name]);
+      }
+      return this;
+    }
+    var previous = this.get(key);
+    if( previous === val ) return; // No change
+    this._setPath(key, val);
+    this.emit('change', key, val, previous);
+    this.emit('change'+key, val, previous);
+    return this;
+  };
 
-Observable.prototype.get = function(key) {
-  return this._getPath(key);
-};
+  Observable.prototype._setPath = function(path, value) {
+    var parts = path.split('.');
+    var target = this.attributes;
+    var last = parts.pop();
+    while(parts.length) {
+      part = parts.shift();
+      if(!target[part]) {
+        target[part] = {};
+      }
+      target = target[part];
+    }
+    target[last] = value;
+    this.emit('change:'+path, value, previousValue);
+  };
 
-exports = module.exports = function(obj){
-  return new Observable(obj);
+  Observable.prototype._getPath = function(path) {
+    var parts = path.split('.');
+    var value = this.attributes;
+    while(parts.length) {
+      var part = parts.shift();
+      value = value[part];
+      if(value === undefined) parts.length = 0;
+    }
+    return value;
+  };
+
+  Observable.prototype.get = function(key) {
+    return this._getPath(key);
+  };
+
+  return Observable;
 };
 
 exports.Observable = Observable;
