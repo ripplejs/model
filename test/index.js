e52e335ae7345bc141003c3a4a3b92876e7656f5
Anthony Short <antshort@gmail.com>
2014-03-05 21:31:07 -0800

    Fixed tests

--- test/index.js
+++ test/index.js
@@ -140,44 +140,6 @@ describe('Model', function(){
 
   })
 
-  it('should be able to do computed properties', function(){
-    Model.computed('three', ['one', 'two'], function(){
-      return this.get('one') + this.get('two')
-    });
-    model = new Model({
-      one: 1,
-      two: 2
-    });
-    assert(model.get('three') === 3);
-  })
-
-  it('should pass the values through to the callback', function(){
-    Model.computed('three', ['one', 'two'], function(one, two){
-      return one + two;
-    });
-    model = new Model({
-      one: 1,
-      two: 2
-    });
-    model.set('two', 3);
-    assert(model.get('three') === 4);
-  })
-
-  it('should emit change events for computed properties', function(done){
-    Model.computed('three', ['one', 'two'], function(){
-      return this.get('one') + this.get('two')
-    });
-    model = new Model({
-      one: 1,
-      two: 2
-    });
-    model.change('three', function(change){
-      assert(change === 4);
-      done();
-    });
-    model.set('one', 2);
-  })
-
   it('should use the change method for binding to changes', function(done){
     model = new Model();
     model.change('one', function(change){
